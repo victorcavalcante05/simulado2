25767028014fae4261d5c39a6f4feb3c70ce74b5
Laerte Jun Takeuti <laertejt@gmail.com>
2024-09-17 16:51:48 -0300

    inclusao de simulado

---
+++ README.md
@@ -0,0 +1,16 @@
+# Descrição
+Simulação da Avaliação Parcial 1
+
+## Instituição
+Essa é uma aula fornecida no curso de Administração e Economia do IBMEC - Brasília.
+
+## Curso
+Conceitos fornecidos durante a disciplina Progamação para Análise de Dados do 2o.Semestre de 2024.
+
+## Professor
+Laerte Jun Takeuti, MsC \
+email: laerte.takeuti@professores.ibmec.edu.br \
+linkedin: https://www.linkedin.com/in/laertejt/ \
+site: https://laboratoriodefinancas.com 
+
+## Aluno
---
+++ simulado_prova.py
@@ -0,0 +1,33 @@
+# 1- Faca um for loop no dicionario e exiba os valores de cada tipo: 
+tipos_cores = {
+    'primaria': ['vermelho', 'azul', 'amarelo'],
+    'secundaria': ['verde', 'laranja', 'roxo'],
+    }
+
+
+# 2 - Faca um for loop na lista e mostre qual tipo ela é
+lst_cores = ['vermelho', 'roxo', 'preto'] 
+
+
+
+
+
+# 3 – Verifique o total da compra baseado no preco
+precos = {
+    'camiseta': 100.00,
+    'tenis': 900.00,
+    'meia': 45.00,
+    'blusa': 245.00,
+    'calça': 145.00,
+    'luva': 18.00,
+    }
+
+compra = ['luva', 'blusa', 'tenis']
+
+
+# 4 – Faca uma funcao que entre 3 notas e calcule a media final de acordo com o crirterio de nota do ibmec
+# Crie uma pasta meu_pacote e grava essa funcao la dentro
+# Importe a funcao, e chame ela, calculando a media final
+p1 = 8.5
+p2 = 9.0
+ac = 1.0
